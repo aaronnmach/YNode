b0f43137f625d08876458f21bdcbd72c88ec9f8a
amach8 <98847891+amach8@users.noreply.github.com>
2024-10-22 19:20:18 -0700

    added dependencies

---
+++ rust_blockchain/target/debug/deps/rust_blockchain-09c0fefe2b41103f.d
@@ -0,0 +1,5 @@
+/Users/aaronnmsch/Desktop/YNode2.0/YNode/rust_blockchain/target/debug/deps/rust_blockchain-09c0fefe2b41103f: src/main.rs
+
+/Users/aaronnmsch/Desktop/YNode2.0/YNode/rust_blockchain/target/debug/deps/rust_blockchain-09c0fefe2b41103f.d: src/main.rs
+
+src/main.rs:
